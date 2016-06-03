b8e7d9171ab4ebcff5f00cc3faf534adbb871815
Christian Maniewski <chmanie@fastmail.com>
2016-06-03 02:47:55 +0200

    0.0.28

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION=${1:-master} # e.g. 0.0.18
+rm -rf $DIR/../bulma
+cd $DIR/..
+git clone https://github.com/jgthms/bulma.git
 cd $DIR/../bulma
-git fetch upstream --tags
+git fetch origin --tags
 git checkout $VERSION
 find . -name '*.sass' -exec sass-convert -F sass -T scss '{}' '{}.temp' \;
 find . -name '*.sass.temp' -exec rename 's/^(.+)\/(.+?).sass.temp$/$1\/_$2.scss/' '{}' \;
 find . -name '*.sass' -type f -delete
 cd $DIR/..
 rm -rf dist
-mkdir -p dist/bulma
-mv bulma/bulma dist
+mkdir -p dist
+mv bulma/sass dist
 mv bulma/_bulma.scss dist
 cd $DIR/../bulma
 git reset --hard
